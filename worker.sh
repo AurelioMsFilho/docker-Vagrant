a8c70fa1a12132cfc50c2ec1898ca03cf11f46b6
Aurélio Martins Filho <89323886+AurelioMsFilho@users.noreply.github.com>
2023-07-31 20:47:02 -0300

    Atualizar o worker.sh

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-1ajawb85ksbzdmo8xe02nyfekl1w3dqo8rvqfc8a6q7df6t10q-18o78gbku5opfeqfwx8udat0u 192.168.56.100:2377
+    docker swarm join --token SWMTKN-1-1ajawb85ksbzdmo8xe02nyfekl1w3dqo-18o78gbku5opfeqfwx8udat0u 192.168.56.100:2377
