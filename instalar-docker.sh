e64cd8ea1abe8af961493ab3c2297bff8f896075
Aurélio Martins Filho <Aureliofilhotj@gmail.com>
2023-07-31 18:21:24 -0300

    Projeto_Cluster

---
+++ instalar-docker.sh
@@ -0,0 +1,10 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh ./get-docker.sh --dry-run
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install default-jre
+sudo apt-get -y install default-jdk
+sudo apt-get -y install tomcat7
+sudo service tomcat7 start
